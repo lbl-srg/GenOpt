d7c2ca62f03d6faae582fc3e1ed81d2fed95a2eb
Michael Wetter <mwetter@lbl.gov>
2008-11-15 06:20:44 +0000

    Fixed initialization for second algorithm

--- src/genopt/algorithm/GPSPSOCCHJ.java
+++ src/genopt/algorithm/GPSPSOCCHJ.java
@@ -127,7 +127,12 @@ public class GPSPSOCCHJ extends PSOCCMesh
 	int retFla = super.run((Point)x0.clone());
 
 	// get initial point for second algorithm
-	x0 = (Point)super.getMinimumPoint();
+	Point xMinIni = (Point)super.getMinimumPoint();
+	x0.setX(xMinIni.getX());
+	x0.setIndex(xMinIni.getIndex());
+	x0.setF(xMinIni.getF()); // set the function value to get correct reporting of initial value
+	x0.setStepNumber(xMinIni.getStepNumber());
+	x0.setSimulationNumber(xMinIni.getSimulationNumber());
 	x0.setComment("Minimum point of initialization.");
 	report(x0, Optimizer.SUBITERATION);
 	report(x0, Optimizer.MAINITERATION);
