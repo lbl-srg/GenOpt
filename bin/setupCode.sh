c4f1f5521bfbe3e440f0ef1916e8b63f0aae7757
Michael Wetter <mwetter@lbl.gov>
2011-12-08 18:45:04 +0000

    Updated setupCode.sh

--- bin/setupCode.sh
+++ bin/setupCode.sh
@@ -2,20 +2,20 @@
 FILES=`find src -name *.java`
 for ff in $FILES; do
     echo $ff
-    sed -e 's/GenOpt(R) 3.0.2/GenOpt(R) 3.0.3/g' $ff > $ff.AAAA
+    sed -e 's/GenOpt(R) 3.0.3/GenOpt(R) 3.1.0/g' $ff > $ff.AAAA
     mv $ff.AAAA $ff
-    sed -e 's/(November 6, 2009)/(April 26, 2010)/g' $ff > $ff.AAAA
+    sed -e 's/(November 30, 2011)/(December 8, 2011)/g' $ff > $ff.AAAA
     mv $ff.AAAA $ff
 done
 
 FILES=`find ./ \( -name '*.java' -or -name '*.html' \)`
 for ff in $FILES; do
-    sed -e 's/GenOpt Copyright (c) 1998-2009/GenOpt Copyright (c) 1998-2010/g' $ff > $ff.AAAA
+    sed -e 's/GenOpt Copyright (c) 1998-2010/GenOpt Copyright (c) 1998-2011/g' $ff > $ff.AAAA
     mv $ff.AAAA $ff
 done
 
 FIL=install/info.html
-sed -e 's/3.0.2/3.0.3/g' $FIL > $FIL.AAAA
+sed -e 's/3.0.3/3.1.0/g' $FIL > $FIL.AAAA
 if [ "$?" != "0" ]; then
     echo "Error in replacing string in $FIL"
     echo "Exit with error"
@@ -25,7 +25,7 @@ mv $FIL.AAAA $FIL
 
 
 FIL=install/install.xml
-sed -e 's/3.0.2/3.0.3/g' $FIL > $FIL.AAAA
+sed -e 's/3.0.3/3.1.0/g' $FIL > $FIL.AAAA
 if [ "$?" != "0" ]; then
     echo "Error in replacing string in $FIL"
     echo "Exit with error"
